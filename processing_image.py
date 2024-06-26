18bf7c52c031837c9184a93d3cfc2cd7c206d475
flavieq88 <qinflavie@gmail.com>
2024-06-26 19:23:39 -0400

    reload model

--- processing_image.py
+++ processing_image.py
@@ -1,2 +1,6 @@
-# this file is to process an image of handwritten digits to be able to be fed through the digit classifier
+# to process an image of handwritten digits, to be able to be fed through the digit classifier
+
 import cv2
+import keras
+
+model = keras.models.load_model("classifier_model/model.keras")
