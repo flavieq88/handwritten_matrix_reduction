36e2fbf3c2cc9e6ee723b6e61013bb61693158f4
flavieq88 <qinflavie@gmail.com>
2024-06-26 20:36:40 -0400

    tested with my handwriting

--- processing_image.py
+++ processing_image.py
@@ -2,5 +2,37 @@
 
 import cv2
 import keras
+import matplotlib.pyplot as plt
+import numpy as np
+import os
 
-model = keras.models.load_model("classifier_model/model.keras")
+def predictImage(filepath, classifier):
+    """Reads the image at the specified filepath and returns a predicted label of the digit found by the classifier model"""
+    image = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+    image = cv2.resize(255-image, (28, 28)) #invert colours and make it 28x28
+    (thresh, image) = cv2.threshold(image, 5, 255, cv2.ADAPTIVE_THRESH_MEAN_C)
+    image = image/255 #normalize pixel value ranges to be 0-1
+    pred = classifier.predict(np.array([image,]))
+    plt.figure()
+    plt.xticks()
+    plt.yticks()
+    plt.imshow(image)
+    plt.xlabel(f"True label: {filepath.strip("testing_images/").strip(".png")}\n Predicted label: {np.argmax(pred)}")
+    plt.show()
+    return np.argmax(pred)
+
+if __name__ == "__main__":
+#have all the testing images in one list
+    ls = [] #store the file names
+    predictions = [] #store the predictions
+    directory = "testing_images"
+    for filename in os.listdir(directory):
+        ls.append(filename)
+
+    #load the classifier 
+    model = keras.models.load_model("classifier_model/model.keras")
+
+    for file in ls:
+        predictions.append(predictImage(directory+"/"+file, model))
+
+    print(predictions)
